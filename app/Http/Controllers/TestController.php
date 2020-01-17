87b38dba3d712a75b374c2b74dd19909cd995eb2
Mark Andrew Colocado <mark05@simplexi.com>
2020-01-17 15:47:57 +0800

    Added migrations.

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Cli
+/**
+ * Class TestController
+ *
+ * @package App\Http\Controllers;
+ *
+ * @author Mark Andrew Colocado <mark05@simplexi.com.ph>
+ * @date 1/17/2020 3:46 PM
+ */
+class TestController
+{
+    public function getTest()
+    {
+
+    }
+}
